bae632812e8524459fa16ad64578f71c273f93f6
Hajid Raihan <hajidraihan@gmail.com>
2024-03-06 13:12:25 +0800

    add migration

---
+++ database/migrations/2024_03_06_045613_create_activity_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activity', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->enum('company', ['jtse', 'mmn']);
+            $table->date('tanggal');
+            $table->string('jenis_hardware');
+            $table->string('standart_aplikasi');
+            $table->string('uraian_hardware');
+            $table->string('uraian_aplikasi');
+            $table->string('aplikasi_it_tol');
+            $table->string('uraian_it_tol');
+            $table->string('catatan');
+            $table->string('shift');
+            $table->unsignedBigInteger('lokasi_id');
+            $table->unsignedBigInteger('kategori_id');
+            $table->string('kondisi_akhir');
+            $table->integer('biaya');
+            $table->string('fotos');
+            $table->enum('status', ['prosses', 'done']);
+            $table->timestamp('ended_at');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade');
+            $table->foreign('lokasi_id')->references('id')->on('lokasi')->onUpdate('cascade');
+            $table->foreign('kategori_id')->references('id')->on('kategori')->onUpdate('cascade');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * user_id integer [primary key]
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activity');
+    }
+};
