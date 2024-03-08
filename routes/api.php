f9c42045a270100e31ee4bc53ebb353ccc7a6317
AndiMaghfirahInzani <firahinzaniii@gmail.com>
2024-03-08 10:56:27 +0800

    add api kategori and location crud

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LokasiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +24,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('register', [AuthenticationController::class, 'register']);
 Route::post('login', [AuthenticationController::class, 'login']);
 Route::post('logout', [AuthenticationController::class, 'logout']);
+
+
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::post('/kategori', [KategoriController::class, 'store']);
+Route::get('/kategori/{id}', [KategoriController::class, 'show']);
+Route::put('/kategori/{id}', [KategoriController::class, 'update']);
+Route::delete('/kategori/{id}', [KategoriController::class, 'destroy']);
+
+Route::get('/lokasi', [LokasiController::class, 'index']);
+Route::post('/lokasi', [LokasiController::class, 'store']);
+Route::get('/lokasi/{id}', [LokasiController::class, 'show']);
+Route::put('/lokasi/{id}', [LokasiController::class, 'update']);
+Route::delete('/lokasi/{id}', [LokasiController::class, 'destroy']);
+
+Route::get('/barang', [BarangController::class, 'index']);
+Route::post('/barang', [BarangController::class, 'store']);
+Route::get('/barang/{id}', [BarangController::class, 'show']);
+Route::put('/barang/{id}', [BarangController::class, 'update']);
+Route::delete('/barang/{id}', [BarangController::class, 'destroy']);
+
