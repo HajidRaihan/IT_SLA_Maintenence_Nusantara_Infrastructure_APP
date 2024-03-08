b08cc93f85128778f43987cc9d5471cfd6c1e67f
Dirgan Boro <dirganoob>
2024-03-08 10:47:25 +0800

    add api activity crud

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\ActivityController; // Add this line
-
+use App\Http\Controllers\WebActivityController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,7 +23,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('register', [AuthenticationController::class, 'register']);
 Route::post('login', [AuthenticationController::class, 'login']);
 Route::post('logout', [AuthenticationController::class, 'logout']);
-Route::post('activities', [ActivityController::class, 'activity']); 
+Route::post('activities', [ActivityController::class, 'addactivity_toll']); 
+Route::get('activities', [ActivityController::class, 'getactivity_toll']); 
 Route::put('activities/edit/{id}', [ActivityController::class, 'edit_activity']);
 Route::delete('activities/delete/{id}', [ActivityController::class, 'delete_activity']); 
+Route::get('/toll-form', [WebActivityController::class, 'showForm'])->name('toll.form');
+
 
