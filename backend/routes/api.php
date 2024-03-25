fe09453a4b7cc433bd6348dcba8a6350074015d2
Dirgan Boro <dirganoob>
2024-03-25 13:58:49 +0800

    add change status on api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LokasiController;
 use App\Http\Controllers\BarangController;
-use App\Http\Controllers\JadwalController;
 
 
 /*
@@ -30,29 +29,20 @@ Route::post('/register', [AuthenticationController::class, 'register']);
 Route::post('/login', [AuthenticationController::class, 'login']);
 
 
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () { 
     Route::get('logout', [AuthenticationController::class, 'logout']);
-});
-Route::get('/tes', function () {
-    return "tes";
-});
-
-Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users', [UserController::class, 'index']);
 Route::post('/users/update/{id}', [UserController::class, 'updateProfile']);
 
 Route::post('/toll', [ActivityController::class,'addactivity_toll']);
+Route::post('/status', [ActivityController::class,'changeStatus']);
 Route::get('/toll', [ActivityController::class,'getactivity_toll']);
-Route::get('/toll/{id}', [ActivityController::class, 'getactivity_toll_id']);
-// Route::get('/toll/user/{userId}', [ActivityController::class, 'getactivity_toll_by_user']);
 Route::put('/toll/update/{id}', [ActivityController::class,'edit_activity']);
 Route::delete('/toll/delete/{id}', [ActivityController::class,'delete_activity']);
 Route::post('/nontoll', [ActivityController::class,'addactivity_nontoll']);
 Route::get('/nontoll', [ActivityController::class,'getactivity_nontoll']);
-Route::get('/nontoll/{id}', [ActivityController::class, 'getactivity_nontoll_id']);
-// Route::get('/nontoll/user/{userId}', [ActivityController::class, 'getactivity_nontoll_by_user']);
 Route::put('/nontoll/update/{id}', [ActivityController::class,'edit_activitynontoll']);
 Route::delete('/nontoll/delete/{id}', [ActivityController::class,'delete_activitynontoll']);
-Route::put('/toll/{id}/status', [ActivityController::class,'changeStatus']);
 
 
 Route::get('/kategori', [KategoriController::class, 'index']);
@@ -72,12 +62,12 @@ Route::post('/barang', [BarangController::class, 'store']);
 Route::get('/barang/{id}', [BarangController::class, 'show']);
 Route::put('/barang/{id}', [BarangController::class, 'update']);
 Route::delete('/barang/{id}', [BarangController::class, 'destroy']);
+});
+Route::get('/tes', function () {
+    return "tes";
+});
 
-Route::get('/jadwal', [JadwalController::class, 'index']);
-Route::post('/jadwal', [JadwalController::class, 'store']);
-Route::get('/jadwal/{id}', [JadwalController::class, 'show']);
-Route::put('/jadwal/{id}', [JadwalController::class, 'update']);
-Route::delete('/jadwal/{id}', [JadwalController::class, 'destroy']);
+
 
 
 
